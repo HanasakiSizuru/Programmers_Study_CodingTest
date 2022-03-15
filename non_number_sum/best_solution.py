b112821d5efeffad42598078862334bfc01dc3b6
HanasakiSizuru <kkej1009@naver.com>
2022-03-15 12:38:03 +0900

    최고의 풀이

---
+++ non_number_sum/best_solution.py
@@ -0,0 +1,11 @@
+def solution(nnumbers):
+    return 45 - sum(numbers)
+
+def main():
+    ns = [1,2,3,4,6,7,8,0]
+    print(solution(ns))
+
+    ns = [5,8,4,0,6,7,9]
+    print(solution(ns))
+
+main()
